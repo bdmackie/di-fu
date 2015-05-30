fb1034d7e80339abfc3da3da9e5a084ffea829fd
Ben Mackie <bmackie@atlassian.com>
2015-05-31 00:53:14 +1000

    removed require

--- examples/binding.js
+++ examples/binding.js
@@ -2,19 +2,25 @@ var di = require('../index').container();
 //var di = require('di-fu').container();
 console.log(di.has('foo')); // false
 
+// A basic bind call to register a component.
 di.bind('foo').to({
 	hello: function() { return "hello world"; }
 });
 console.log(di.has('foo')); // true
 
-var foo = di.get('foo');
+// The module defaults to a 'get' function. You can 
+// call di.get explicitly if you prefer.
+var foo = di('foo');
 console.log(foo.hello()); // hello world
 
+// Call rebind to register a component without
+// causing an error - e.g. for mocking.
 di.rebind('foo').to({
 	hello: function() { return "goodbye"; }
 });
 var foo = di.get('foo');
 console.log(foo.hello()); // goodbye
 
+// Components can be unbound if desired.
 di.unbind('foo');
 console.log(di.has('foo')); // false
