644e87ee5b021f71f1a3a0eb5998db0efabc922e
Ben Mackie <bdmackie@gmail.com>
2015-05-31 01:42:35 +1000

    maintenance

--- examples/invoking.js
+++ examples/invoking.js
@@ -2,7 +2,27 @@ var di = require('../index').container();
 //var di = require('di-fu').container();
 console.log(di.has('foo')); // false
 
-di.bind('foo').to(function() { return "hello world"; });
+// Bind a simple function.
+di.bind('foo').to(function(name) {
+	return "Hello " + name; 
+});
 console.log(di.has('foo')); // true
 
-console.log(di.call('foo')); // hello world
+// Invoke the function.
+console.log(di.invoke('foo', ['Bob'])); // Hello Bob
+
+// Bind a simple class.
+di.bind('Bar').to(function(name) {
+	var _this = this;
+	var _name = name;
+
+	_this.hello = function() {
+		return "Hello " + _name;
+	}
+});
+console.log(di.has('Bar')); // true
+
+// Create an instance of the class and call it.
+var bar = di.create('Bar', ['Bob']);
+console.log(bar.hello()); // Hello Bob
+
